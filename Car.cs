e9630f8ea1b7aa43b55d198992b7f8cdc94b69e1
Esther <akinsoug@uncc.edu>
2019-08-08 19:19:23 -0400

    adding Car class and main class

---
+++ Car.cs
@@ -0,0 +1,62 @@
+﻿/*
+ *Attributes and Properties:
+ year_model (for the car’s year_model)
+ make (for the make of the car)
+ speed (for the cars current speed)
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarMainDriver
+{
+    public class Car
+    {
+        public int year { get; set; }
+        public string make { get; set; }
+        public int speed { get; set; }
+        public Car()
+        {
+        }
+        /// <summary>
+        /// The car class must have a method that accepts the car’s year_model and make as arguments. 
+        /// These values should be assigned to the car’s year_model and make data attributes.
+        /// It should also assign zero to the speed data attribute.
+        /// </summary>
+        /// <param name="cyear"></param>
+        /// <param name="cmake"></param>
+        public void carMethod(int cyear, string cmake)
+        {
+            this.year = cyear;
+            this.make = cmake;
+            this.speed = 0;
+        }
+
+        /// <summary>
+        /// The accelerate method should and five to the speed data attribute each time it is called.
+        /// </summary>
+        public void accelerate()
+        {
+            this.speed += 5;
+        }
+        
+        /// <summary>
+        /// The break method should subtract five from the speed data attribute each time it called.
+        /// </summary>
+        public void breakk()
+        {
+            this.speed -= 5;
+        }
+
+        /// <summary>
+        /// The current_speed method should return the current speed.
+        /// </summary>
+        /// <returns></returns>
+        public int speedMethod()
+        {
+            return this.speed;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Name: Esther
+ *C# app Car Class Activity.
+ */
+
+using System;
+
+namespace CarMainDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("\t\tName: Adjoavi Kinsougbo (Esther)\n");
+            Console.WriteLine("\t\tWelcome to the Car Class Activity!");
+
+            //instantiating a Car object.
+            Car myCar2 = new Car();
+
+            Console.WriteLine("\t\nCalling the acelerator method");
+            for (int i = 0; i < 5; i++)
+            {
+                myCar2.accelerate();//the acelerator call
+                Console.WriteLine("  {0}\tThe speed is: {1}",i, myCar2.speedMethod());
+            }
+
+            Console.WriteLine("\t\nCalling the break method");
+            for (int i = 0; i < 5; i++)
+            {
+                myCar2.breakk();//the acelerator call
+                Console.WriteLine("  {0}\tThe speed is: {1}", i, myCar2.speedMethod());
+            }
+
+            //instantiating another Car object.
+            Car myCar1 = new Car()
+            {
+                year = 2000,
+                make = "Toyota",
+                speed = 5
+            };
+            Console.Read();
+        }
+    }
+}
